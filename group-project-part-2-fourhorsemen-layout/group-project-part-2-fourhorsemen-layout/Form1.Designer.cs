0fa70fd3348f530a80ea9adf890bac8f4bbe8603
deucemaccoy <155991183+deucemaccoy@users.noreply.github.com>
2025-03-30 13:01:32 -0400

    Add files via upload

---
+++ group-project-part-2-fourhorsemen-layout/group-project-part-2-fourhorsemen-layout/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace group_project_part_2_fourhorsemen_layout
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hpButton = new System.Windows.Forms.Button();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.searchBox = new System.Windows.Forms.TextBox();
+            this.searchTypeBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.resultsBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // hpButton
+            // 
+            this.hpButton.Location = new System.Drawing.Point(22, 66);
+            this.hpButton.Name = "hpButton";
+            this.hpButton.Size = new System.Drawing.Size(96, 32);
+            this.hpButton.TabIndex = 0;
+            this.hpButton.Text = "Homepage";
+            this.hpButton.UseVisualStyleBackColor = true;
+            this.hpButton.Click += new System.EventHandler(this.hpButton_Click);
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(513, 65);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 30);
+            this.searchButton.TabIndex = 1;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(594, 65);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 30);
+            this.clearButton.TabIndex = 2;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // searchBox
+            // 
+            this.searchBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchBox.Location = new System.Drawing.Point(124, 68);
+            this.searchBox.Name = "searchBox";
+            this.searchBox.Size = new System.Drawing.Size(256, 27);
+            this.searchBox.TabIndex = 3;
+            // 
+            // searchTypeBox
+            // 
+            this.searchTypeBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchTypeBox.FormattingEnabled = true;
+            this.searchTypeBox.Location = new System.Drawing.Point(386, 68);
+            this.searchTypeBox.Name = "searchTypeBox";
+            this.searchTypeBox.Size = new System.Drawing.Size(121, 28);
+            this.searchTypeBox.TabIndex = 4;
+            this.searchTypeBox.SelectedIndexChanged += new System.EventHandler(this.searchTypeBox_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.LightSalmon;
+            this.label1.Location = new System.Drawing.Point(163, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(344, 32);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "4H Video Game Database";
+            // 
+            // resultsBox
+            // 
+            this.resultsBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultsBox.Location = new System.Drawing.Point(60, 212);
+            this.resultsBox.Name = "resultsBox";
+            this.resultsBox.ReadOnly = true;
+            this.resultsBox.Size = new System.Drawing.Size(528, 155);
+            this.resultsBox.TabIndex = 6;
+            this.resultsBox.Text = "";
+            this.resultsBox.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::group_project_part_2_fourhorsemen_layout.Properties.Resources.fourhorsemen_washout;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(700, 686);
+            this.Controls.Add(this.resultsBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.searchTypeBox);
+            this.Controls.Add(this.searchBox);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.hpButton);
+            this.Name = "Form1";
+            this.Text = "Layout Proposal";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button hpButton;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.TextBox searchBox;
+        private System.Windows.Forms.ComboBox searchTypeBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox resultsBox;
+    }
+}
+
---
+++ group-project-part-2-fourhorsemen-layout/group-project-part-2-fourhorsemen-layout/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace group_project_part_2_fourhorsemen_layout
+{
+    public partial class Form1 : Form
+    {
+
+        public String sText, sType;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            searchTypeBox.Items.Add("Game System");
+            searchTypeBox.Items.Add("Title");
+            searchTypeBox.Items.Add("Genre");
+            searchTypeBox.Items.Add("Developer");
+            searchTypeBox.Items.Add("Year of Release");
+
+            sType = "All";
+        }
+
+        private void clear()
+        {
+            searchBox.Text = "";
+            searchTypeBox.SelectedIndex = -1;
+            resultsBox.Text = "";
+            resultsBox.Visible = false;
+            sType = "All";
+        }
+
+        private void hpButton_Click(object sender, EventArgs e)
+        {
+            resultsBox.Text = "";
+            resultsBox.Visible = false;
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            clear();
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            sText = searchBox.Text;
+            resultsBox.Text = "You searched for [" + sText + "] by <" + sType + ">\n\nThis information will be available very soon. Please be patient whuile we build the best Video game database on the web!!!";
+            resultsBox.Visible = true;
+        }
+
+        private void searchTypeBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            sType = (String)searchTypeBox.SelectedItem;
+        }
+    }
+}
