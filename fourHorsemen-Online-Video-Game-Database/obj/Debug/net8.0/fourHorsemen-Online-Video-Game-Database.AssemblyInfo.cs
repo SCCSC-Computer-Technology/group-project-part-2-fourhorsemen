8a14e6078bca8f712e574fca435e16080b1f4be0
Jared Daniels <jareddaniels2935@gmail.com>
2025-04-17 11:50:14 -0400

    Stylized details page (can be changed if you don't like it)

--- fourHorsemen-Online-Video-Game-Database/obj/Debug/net8.0/fourHorsemen-Online-Video-Game-Database.AssemblyInfo.cs
+++ fourHorsemen-Online-Video-Game-Database/obj/Debug/net8.0/fourHorsemen-Online-Video-Game-Database.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fourHorsemen-Online-Video-Game-Database")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb272a32de25315e2585fff9ee9e07eb04d6b718")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d03a2ab273d9d43cdf3ebd028b6ceb7ff1f47e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("fourHorsemen-Online-Video-Game-Database")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fourHorsemen-Online-Video-Game-Database")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
