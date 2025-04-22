b437c998fe3cb2a95576ef381f181da023b865b3
SUPARedBeard <joshpitts90@gmail.com>
2025-04-21 20:57:02 -0400

    MDF Fixed

--- fourHorsemen-Online-Video-Game-Database/obj/Debug/net8.0/fourHorsemen-Online-Video-Game-Database.AssemblyInfo.cs
+++ fourHorsemen-Online-Video-Game-Database/obj/Debug/net8.0/fourHorsemen-Online-Video-Game-Database.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fourHorsemen-Online-Video-Game-Database")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1e02e1bc4551e8c668ea512031216243d1a916f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+797266e8b523d6217213876658dabd3c570806e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("fourHorsemen-Online-Video-Game-Database")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fourHorsemen-Online-Video-Game-Database")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
