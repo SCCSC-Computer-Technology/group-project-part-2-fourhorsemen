c4206e78f5d6383421a4b448c10d2c19a644cded
CodeMonkeyDan <dmschiefer@gmail.com>
2025-04-24 22:41:01 -0400

    Updated Nav Bar and Footer
    Updated nav bar to be 2 step
    
    Fixed footer issue where it was taking the page style instead of the layout style on game list view

--- fourHorsemen-Online-Video-Game-Database/obj/Debug/net8.0/fourHorsemen-Online-Video-Game-Database.AssemblyInfo.cs
+++ fourHorsemen-Online-Video-Game-Database/obj/Debug/net8.0/fourHorsemen-Online-Video-Game-Database.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fourHorsemen-Online-Video-Game-Database")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e33c4ac7c44993dfb4619c960c0f42b07f33740b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f92705665749729bf5ce7f3fc49b8ec355abfdf")]
 [assembly: System.Reflection.AssemblyProductAttribute("fourHorsemen-Online-Video-Game-Database")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fourHorsemen-Online-Video-Game-Database")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
