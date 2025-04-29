dcaae136bec7cc5f2e2e3a4f2c7aa9f5028d0fdc
CodeMonkeyDan <dmschiefer@gmail.com>
2025-04-28 20:08:08 -0400

    Updated My Profile
    Updated my avatar and username

--- fourHorsemen-Online-Video-Game-Database/obj/Debug/net8.0/fourHorsemen-Online-Video-Game-Database.AssemblyInfo.cs
+++ fourHorsemen-Online-Video-Game-Database/obj/Debug/net8.0/fourHorsemen-Online-Video-Game-Database.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fourHorsemen-Online-Video-Game-Database")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14017d3bf0ef48b9cb4832310458ae3ba92d055a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dafa62ed726eab31a0650e7f582a0dcc0309d941")]
 [assembly: System.Reflection.AssemblyProductAttribute("fourHorsemen-Online-Video-Game-Database")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fourHorsemen-Online-Video-Game-Database")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
